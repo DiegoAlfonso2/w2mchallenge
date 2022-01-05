5c36230e658adf4198c93358c7a6d1e6f670b369
Diego Alfonso <diego.alfonso2@gmail.com>
2022-01-05 03:54:55 -0300

    Primer test de /login pasando

--- src/main/java/com/w2m/challenge/superhero/configuration/WebSecurityConfig.java
+++ src/main/java/com/w2m/challenge/superhero/configuration/WebSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.w2m.challenge.superhero.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -15,6 +20,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		http.csrf().disable()
 			.authorizeRequests()
 				.antMatchers("/login").permitAll()
-				.antMatchers("/heroes/**").authenticated();
+				.antMatchers("/heroes/**").authenticated()
+			.and()
+			.exceptionHandling()
+				.defaultAuthenticationEntryPointFor(unauthorizedEntryPoint(), new AntPathRequestMatcher("/heroes/**"));
+	}
+	
+	@Bean
+	public AuthenticationEntryPoint unauthorizedEntryPoint() {
+		return new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED);
 	}
 }
