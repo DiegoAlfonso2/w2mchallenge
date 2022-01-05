2167a78aa41433b8c8705f79845048c661efaa78
Diego Alfonso <diego.alfonso2@gmail.com>
2022-01-05 03:55:50 -0300

    Refactor menor: lombok instalado en el IDE me permite sacar getters y setters

--- src/main/java/com/w2m/challenge/superhero/dto/AuthTokenDTO.java
+++ src/main/java/com/w2m/challenge/superhero/dto/AuthTokenDTO.java
@@ -6,12 +6,4 @@ import lombok.Data;
 public class AuthTokenDTO {
 
 	private String token;
-
-	public String getToken() {
-		return token;
-	}
-
-	public void setToken(String token) {
-		this.token = token;
-	}
 }
