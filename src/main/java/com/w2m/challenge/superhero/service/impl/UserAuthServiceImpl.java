9601d76ee57cfc1f6c3fd98a305508cf20c02db2
Diego Alfonso <diego.alfonso2@gmail.com>
2022-01-07 23:29:12 -0300

    Primer test del servicio de autenticacion pasando

--- src/main/java/com/w2m/challenge/superhero/service/impl/UserAuthServiceImpl.java
+++ src/main/java/com/w2m/challenge/superhero/service/impl/UserAuthServiceImpl.java
@@ -1,17 +1,22 @@
 package com.w2m.challenge.superhero.service.impl;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
 import com.w2m.challenge.superhero.repository.UserRepository;
 import com.w2m.challenge.superhero.service.UserAuthService;
 
 public class UserAuthServiceImpl implements UserAuthService {
 
+	private UserRepository userRepository;
+	
 	public UserAuthServiceImpl(UserRepository userRepository) {
-		// TODO Auto-generated constructor stub
+		this.userRepository = userRepository;
 	}
 
 	public String authenticate(String username, String password) {
-		// TODO Auto-generated method stub
-		return "";
+		var hashedPassword = DigestUtils.sha256Hex(username + password);
+		var user = userRepository.findByUsernameAndHashedPassword(username, hashedPassword).get();
+		return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
 	}
 
 }
