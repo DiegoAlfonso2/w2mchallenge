001952b2cf9f0fb3287f36b7d2817b0697e8edb2
Diego Alfonso <diego.alfonso2@gmail.com>
2022-01-08 02:19:41 -0300

    Agrego implementacion de JwtTokenServiceImpl que hace pasar el test

--- src/main/java/com/w2m/challenge/superhero/service/impl/JwtTokenServiceImpl.java
+++ src/main/java/com/w2m/challenge/superhero/service/impl/JwtTokenServiceImpl.java
@@ -1,10 +1,34 @@
 package com.w2m.challenge.superhero.service.impl;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Base64;
+import java.util.Date;
+import java.util.Map;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
 public class JwtTokenServiceImpl {
 
 	public String generateTokenFor(String username, String commaSeparatedRoles) {
-		// TODO Auto-generated method stub
-		return null;
+		var now = LocalDateTime.now();
+		Map<String, String> customClaims = Map.of(
+				"username", username,
+				"roles", commaSeparatedRoles);
+		Claims claims = Jwts
+				.claims()
+				.setIssuedAt(Date.from(now.atZone(ZoneId.systemDefault()).toInstant()))
+				.setExpiration(Date.from(now.plusSeconds(60).atZone(ZoneId.systemDefault()).toInstant()));
+		claims.putAll(customClaims);
+		
+		return Jwts
+				.builder()
+				.setHeaderParam("typ", "JWT")
+				.setClaims(claims)
+				.signWith(SignatureAlgorithm.HS256, Base64.getEncoder().encode("secret".getBytes()))
+				.compact();
 	}
 
 }
