869b9873286739c7138333dedd95ea5e2f54d74a
Diego Alfonso <diego.alfonso2@gmail.com>
2022-01-08 02:12:43 -0300

    Agrego test fallando para nuevo servicio JwtTokenServiceImpl, al que movere la logica de manejo de tokens

---
+++ src/main/java/com/w2m/challenge/superhero/service/impl/JwtTokenServiceImpl.java
@@ -0,0 +1,10 @@
+package com.w2m.challenge.superhero.service.impl;
+
+public class JwtTokenServiceImpl {
+
+	public String generateTokenFor(String username, String commaSeparatedRoles) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/test/java/com/w2m/challenge/superhero/service/impl/JwtTokenServiceImplTest.java
@@ -0,0 +1,33 @@
+package com.w2m.challenge.superhero.service.impl;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.text.ParseException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.w2m.challenge.superhero.test.helpers.AuthTokenTestHelper;
+
+@ExtendWith(MockitoExtension.class)
+@RunWith(JUnitPlatform.class)
+public class JwtTokenServiceImplTest implements AuthTokenTestHelper {
+
+	private JwtTokenServiceImpl serviceUnderTest;
+	
+	@BeforeEach
+	public void setUp() {
+		this.serviceUnderTest = new JwtTokenServiceImpl();
+	}
+
+	@Test 
+ 	public void shouldGenerateJwtTokenWithProvidedClaims() throws ParseException {
+		String token = serviceUnderTest.generateTokenFor("pepito", "ROLE_USER");
+		assertTrue(hasClaim(token, "username", "pepito"));
+		assertTrue(hasClaim(token, "roles", "ROLE_USER"));
+	}
+}
