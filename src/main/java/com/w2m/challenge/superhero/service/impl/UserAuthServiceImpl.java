115d52ff28b1f3e32c48440af17a71d1d849793d
Diego Alfonso <diego.alfonso2@gmail.com>
2022-01-08 01:42:08 -0300

    Implementacion mas real del token JWT que hace pasar los tests

--- src/main/java/com/w2m/challenge/superhero/service/impl/UserAuthServiceImpl.java
+++ src/main/java/com/w2m/challenge/superhero/service/impl/UserAuthServiceImpl.java
@@ -1,9 +1,19 @@
 package com.w2m.challenge.superhero.service.impl;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Base64;
+import java.util.Date;
+import java.util.Map;
+
 import org.apache.commons.codec.digest.DigestUtils;
 
 import com.w2m.challenge.superhero.repository.UserRepository;
 import com.w2m.challenge.superhero.service.UserAuthService;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 public class UserAuthServiceImpl implements UserAuthService {
 
@@ -16,7 +26,22 @@ public class UserAuthServiceImpl implements UserAuthService {
 	public String authenticate(String username, String password) {
 		var hashedPassword = DigestUtils.sha256Hex(username + password);
 		var user = userRepository.findByUsernameAndHashedPassword(username, hashedPassword).get();
-		return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+		var now = LocalDateTime.now();
+		Map<String, String> customClaims = Map.of(
+				"username", user.getUsername(),
+				"roles", user.getRoles());
+		Claims claims = Jwts
+				.claims()
+				.setIssuedAt(Date.from(now.atZone(ZoneId.systemDefault()).toInstant()))
+				.setExpiration(Date.from(now.plusSeconds(60).atZone(ZoneId.systemDefault()).toInstant()));
+		claims.putAll(customClaims);
+		
+		return Jwts
+				.builder()
+				.setHeaderParam("typ", "JWT")
+				.setClaims(claims)
+				.signWith(SignatureAlgorithm.HS256, Base64.getEncoder().encode("secret".getBytes()))
+				.compact();
 	}
 
 }
