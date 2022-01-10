8e457bf3da580ec1847c6fb480b524381e5de271
Diego Alfonso <diego.alfonso2@gmail.com>
2022-01-10 00:41:17 -0300

    Agrego una mock implementation que hace pasar el test de JwtTokenAuthenticationProvider

--- src/main/java/com/w2m/challenge/superhero/auth/JwtTokenAuthenticationProvider.java
+++ src/main/java/com/w2m/challenge/superhero/auth/JwtTokenAuthenticationProvider.java
@@ -1,9 +1,14 @@
 package com.w2m.challenge.superhero.auth;
 
+import java.util.Optional;
+
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import com.w2m.challenge.superhero.model.auth.User;
 
 public class JwtTokenAuthenticationProvider extends AbstractUserDetailsAuthenticationProvider {
 
@@ -16,8 +21,14 @@ public class JwtTokenAuthenticationProvider extends AbstractUserDetailsAuthentic
 	@Override
 	protected UserDetails retrieveUser(String username, UsernamePasswordAuthenticationToken authentication)
 			throws AuthenticationException {
-		// TODO Auto-generated method stub
-		return null;
+//		var token = Optional
+//				.ofNullable(authentication.getCredentials())
+//				.map(String::valueOf)
+//				.orElseThrow(() -> new BadCredentialsException("Empty credentials at Authentication Provider"));
+		var user = new User();
+		user.setUsername(username);
+		user.setRoles("ROLE_USER");
+		return user;
 	}
 
 }
--- src/main/java/com/w2m/challenge/superhero/model/auth/User.java
+++ src/main/java/com/w2m/challenge/superhero/model/auth/User.java
@@ -1,8 +1,16 @@
 package com.w2m.challenge.superhero.model.auth;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,10 +21,52 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table(name="User")
-public class User {
+public class User implements UserDetails {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -359457141949423776L;
+
+	private final static String ROLE_SEPARATOR = ",";
 
 	@Id
 	private String username;
 	private String hashedPassword;
 	private String roles;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Arrays.asList(roles.split(ROLE_SEPARATOR))
+				.stream()
+				.map(role -> new SimpleGrantedAuthority(role))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getPassword() {
+		return getHashedPassword();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Currently account expiration is not supported
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Currently account locking is not supported
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Currently credentials expiration is not supported
+		return true;
+	}
+	@Override
+	public boolean isEnabled() {
+		// TODO Currently account disabling is not supported
+		return true;
+	}
 }
