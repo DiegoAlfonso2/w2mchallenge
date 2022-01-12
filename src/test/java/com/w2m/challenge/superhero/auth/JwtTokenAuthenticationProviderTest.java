a0c9a6d8235eef933f429c92534e13be0cc81437
Diego Alfonso <diego.alfonso2@gmail.com>
2022-01-12 01:06:55 -0300

    Agrego test de Provider para aumentar cobertura

--- src/test/java/com/w2m/challenge/superhero/auth/JwtTokenAuthenticationProviderTest.java
+++ src/test/java/com/w2m/challenge/superhero/auth/JwtTokenAuthenticationProviderTest.java
@@ -1,24 +1,23 @@
 package com.w2m.challenge.superhero.auth;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 import com.w2m.challenge.superhero.model.auth.User;
 import com.w2m.challenge.superhero.service.UserAuthService;
 import com.w2m.challenge.superhero.test.helpers.AuthTokenTestHelper;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.mockito.Mockito.when;
-
 @ExtendWith(MockitoExtension.class)
-@RunWith(JUnitPlatform.class)
 public class JwtTokenAuthenticationProviderTest implements AuthTokenTestHelper {
 
 	private JwtTokenAuthenticationProvider providerUnderTest;
@@ -29,14 +28,14 @@ public class JwtTokenAuthenticationProviderTest implements AuthTokenTestHelper {
 	@BeforeEach
 	public void setUp() {
 		providerUnderTest = new JwtTokenAuthenticationProvider(mockUserAuthService);
-		var user = new User();
-		user.setUsername(TEST_USERNAME);
-		user.setRoles(TEST_ROLE_LIST);
-		when(mockUserAuthService.getUserDetailsFromToken(TEST_VALID_TOKEN)).thenReturn(user);
 	}
 	
 	@Test
 	public void shouldRetrieveUserDetailsFromToken() {
+		var user = new User();
+		user.setUsername(TEST_USERNAME);
+		user.setRoles(TEST_ROLE_LIST);
+		when(mockUserAuthService.getUserDetailsFromToken(TEST_VALID_TOKEN)).thenReturn(user);
 		var authentication = new UsernamePasswordAuthenticationToken(TEST_VALID_TOKEN, TEST_VALID_TOKEN);
 		var userDetails = providerUnderTest.retrieveUser(TEST_VALID_TOKEN, authentication);
 		assertThat(userDetails.getUsername(), is("pepito"));
@@ -47,5 +46,13 @@ public class JwtTokenAuthenticationProviderTest implements AuthTokenTestHelper {
 						.map(grantedAuthority -> grantedAuthority.getAuthority())
 						.anyMatch(s -> "ROLE_USER".equalsIgnoreCase(s)), 
 				is(true));
+	}
+	
+	@Test
+	public void shouldThrowBadCredentialsIfTokenLacksCredentials() {
+		var authentication = new UsernamePasswordAuthenticationToken(TEST_VALID_TOKEN, null);
+		assertThrows(BadCredentialsException.class, () -> 
+			providerUnderTest.retrieveUser(TEST_VALID_TOKEN, authentication)
+		);
 	}
 }
