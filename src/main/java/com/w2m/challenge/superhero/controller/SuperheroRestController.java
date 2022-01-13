42aa00a0f9aa0acad7dcd192562a732b32b3d298
Diego Alfonso <diego.alfonso2@gmail.com>
2022-01-13 02:09:05 -0300

    Implementacion basica del endpoint GET /heroes que solo devuelve una lista vacia

--- src/main/java/com/w2m/challenge/superhero/controller/SuperheroRestController.java
+++ src/main/java/com/w2m/challenge/superhero/controller/SuperheroRestController.java
@@ -1,5 +1,20 @@
 package com.w2m.challenge.superhero.controller;
 
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/heroes")
 public class SuperheroRestController {
 
+	@GetMapping
+	public ResponseEntity<List<String> > getHeroes() {
+		return new ResponseEntity<List<String>>(Collections.emptyList(), HttpStatus.OK);
+	}
 }
